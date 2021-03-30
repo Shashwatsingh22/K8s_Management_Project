a4624daa68fd0d5648a33690a492352a4b732520
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2021-03-30 21:55:08 +0530

    some files have been improved

---
+++ web/js/podLabel.js
@@ -0,0 +1,34 @@
+// var imageName= 'kubectl';
+// var service = 'pod';
+// var subService = 'get_all';
+// var tag = "sudo%20kubectl%20get%20pod";
+// var url = `http://master_ip/cgi-bin/main.py?service=${service}&subser=${subService}&tag=${tag}&image_name=${imageName}`
+    
+// GetAll function
+function lab(){
+    var service = 'pod';
+    var subService = 'get_all';
+
+    var url = `http://master_ip/cgi-bin/main.py?service=${service}&subser=${subService}`
+
+    var content = document.createElement('p');
+
+    axios.get(url)
+    .then((response)=>{
+        console.log(response)
+        var value = document.createTextNode(`->  ${response.data}`);
+        content.appendChild(value);
+
+        var element = document.getElementById("divOperationMessage");
+        element.appendChild(content);
+    })
+    .catch((error)=>{
+        console.log(error);
+        
+        var value = document.createTextNode(`->  ${error}`);
+        content.appendChild(value);
+
+        var element = document.getElementById("divOperationMessage");
+        element.appendChild(content);
+    })
+}
